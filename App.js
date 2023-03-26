0c60f87faf1b360b3525447087dc6a7d8d10a15a
gguilhermelopes <guilhermelopes275@gmail.com>
2023-03-26 14:57:09 -0700

    feat: define and load fonts, install dependencies of navigators

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeTabNavigator } from "@react-navigation/bottom-tabs";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "dm-sans": require("./assets/fonts/DMSans-Regular.ttf"),
+    "dm-sans-bold": require("./assets/fonts/DMSans-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    };
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView onLayout={onLayoutRootView} style={styles.rootScreen}>
+        <StatusBar style="auto" />
+        <NavigationContainer></NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
